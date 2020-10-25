a56be0da93dcb2622e912af2947fd59f938a2ba4
CoffeJeanCode <ortizmurciajeanpierre@gmail.com>
2020-10-25 13:43:03 +0000

    fix(file): change name and quit eject script of package.json

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "App";
 
-test('renders learn react link', () => {
+test("testing", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getAllByTitle("Home");
   expect(linkElement).toBeInTheDocument();
 });
