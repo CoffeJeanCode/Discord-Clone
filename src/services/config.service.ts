4e029cf91b9b0cb87780bd8294c5cf604a34563c
Jean Pierre Ortiz Murcia <ortizmurciajeanpierre@gmail.com>
2020-11-04 11:39:04 -0500

    refactor: change project organization

--- src/services/config.service.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const { REACT_APP_API_URL: baseURL } = process.env;
-
-export const apiConfig = axios.create({
-  baseURL,
-});
--- src/test/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "App";
-
-test("testing", () => {
-  render(<App />);
-  const linkElement = screen.getAllByTitle("Home");
-  expect(linkElement).toBeInTheDocument();
-});
